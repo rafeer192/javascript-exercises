d02ab2ad573145f31aa46f3c371b5a9a03e1e881
Rafee <156030423+rafeer192@users.noreply.github.com>
2024-05-16 16:46:33 -0700

    Complete up to exercise 4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let repeated = "";
+  if(num < 0) {
+    repeated = "ERROR"; 
+  } 
+  else {
+    for(let i = 0; i < num; i++) {
+      repeated += string;
+    }
+  }
+  return repeated; 
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...toRemove) {
+  for(const item of toRemove) {
+    let index = array.indexOf(item); 
+    while(index > -1) {
+      array.splice(index, 1); 
+      index = array.indexOf(item); 
+    }
+  }
+  return array; 
 };
 
 // Do not edit below this line
