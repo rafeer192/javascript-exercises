548297ebbb18854c5d9a3dc88b4c4bd12cf2f948
Rafee <156030423+rafeer192@users.noreply.github.com>
2024-05-16 17:48:47 -0700

    Complete exercise 5 sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if(!Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0) //Number.isInteger(value) better than typeof to check if integer
+    return "ERROR"; 
 
+  let sum = 0; 
+  if(num1 > num2) { // Switch num1 and num2 if out of order
+    let temp = num1; 
+    num1 = num2; 
+    num2 = temp; 
+  }
+  for(let i = num1; i <= num2; i++) {
+    sum += i; 
+  }
+  return sum; 
 };
 
 // Do not edit below this line
