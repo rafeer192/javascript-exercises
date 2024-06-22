173de03c1b2765055d2f7fd6e72bfed66e409ae1
Rafee <156030423+rafeer192@users.noreply.github.com>
2024-06-22 15:42:28 -0700

    Complete exercise 12 find the oldest using .reduce()

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+  return array.reduce((prev, current) => {
+    let age = 0; 
+    let prevAge = 0;
+    const date = new Date(); 
+    const year = Number(date.getFullYear()); 
 
+    if(current.yearOfDeath) {
+      age = current.yearOfDeath - current.yearOfBirth; 
+    } else {
+      age = year - current.yearOfBirth; 
+    }
+
+    if(prev.yearOfDeath) {
+      prevAge = prev.yearOfDeath - prev.yearOfBirth; 
+    } else {
+      prevAge = year - prev.yearOfBirth; 
+    }
+    if(age > prevAge) {
+      prev = current; 
+    }
+    return prev; 
+  }); 
 };
 
 // Do not edit below this line
